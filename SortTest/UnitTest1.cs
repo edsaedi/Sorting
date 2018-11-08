0b976242d8438e69f82297b7af145150a7168337
Edan Saedi <edsaedi@gmail.com>
2018-11-07 17:51:11 -0800

    Finished bubble sort

---
+++ SortTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Sorting;
+using System;
+using Xunit;
+
+namespace SortTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void BubbleSort()
+        {
+            for (int x = 0; x < 100; x++)
+            {
+                var nums = Randomize();
+                Sorts<int>.BubbleSort(nums);
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    Assert.True(nums[i] <= nums[i + 1]);
+                }
+            }
+        }
+
+        public int[] Randomize()
+        {
+            int[] temp = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Random rand = new Random();
+                temp[i] = rand.Next(0, 10);
+            }
+            return temp;
+        }
+    }
+}
---
+++ Sorting/Sorts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public static class Sorts<T> where T : IComparable<T>
+    {
+        public static void BubbleSort(T[] array)
+        {
+            bool sorting;
+            do
+            {
+                sorting = false;
+                for (int i = 0; i < array.Length-1; i++)
+                {
+                    if (array[i].CompareTo(array[i + 1]) > 0)
+                    {
+                        var temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                        sorting = true;
+                    }
+                }
+            } while (sorting);
+        }
+    }
+}
